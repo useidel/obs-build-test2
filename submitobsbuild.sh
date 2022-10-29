55a734656f51d83f97d503f29f58e2782b8e654c
Udo Seidel <udoseidel@gmx.de>
2022-10-29 20:31:04 +0200

    added the creation/update of a file to trigger the OBS build every single time

--- submitobsbuild.sh
+++ submitobsbuild.sh
@@ -15,6 +15,9 @@ cd home*
 cp ../*spec dummy/
 cp ../*.tar.gz dummy/
 
+# we create/change a foobar file to trigger the OBS build all the time
+echo $MYDATE > dummy/foobar.txt
+
 # mark files for update if there are new ones
 #cd dummy
 #osc add *spec
@@ -22,6 +25,4 @@ cp ../*.tar.gz dummy/
 
 # upload changed content
 osc ci -m "Update from $MYDATE" dummy
-
-
 
